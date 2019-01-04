4796c2c030c8afe6126242793eb27221f31f37be
Nikhil <nikhil.manapure@wealthtrust.in>
2019-01-04 17:18:28 +0530

    urls added and input.json edited

--- E-Commerce Demo/Utility.m
+++ E-Commerce Demo/Utility.m
@@ -24,7 +24,19 @@
                      @"http://www.stickpng.com/assets/thumbs/58e38ce2204d556bbd97b15f.png",
                      @"http://www.stickpng.com/assets/thumbs/58e38dc4204d556bbd97b166.png",
                      @"http://www.stickpng.com/assets/thumbs/58e38e1a204d556bbd97b167.png",
-                     @"http://www.stickpng.com/assets/thumbs/58e38d0a204d556bbd97b163.png"
+                     @"http://www.stickpng.com/assets/thumbs/58e38d0a204d556bbd97b163.png",
+                     @"http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bf1b.png",
+                     @"http://www.stickpng.com/assets/thumbs/585680404f6ae202fedf26f0.png",
+                     @"http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bf22.png",
+                     @"http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bf28.png",
+                     @"http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bf30.png",
+                     @"http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bf30.png",
+                     @"http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bf31.png",
+                     @"http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bf32.png",
+                     @"http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bf52.png",
+                     @"http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bf53.png",
+                     @"http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bf5a.png",
+                     @"http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bf60.png"
                      , nil];
     return urls[arc4random_uniform((unsigned int)urls.count)];
 }
