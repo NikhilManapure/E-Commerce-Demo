4cec24531878246cedefe67542837e95c8344205
Nikhil Manapure <nikhilmshchs@gmail.com>
2019-07-05 16:20:22 +0530

    trying for web

--- Pods/Target Support Files/Pods-E-Commerce DemoUITests/Pods-E-Commerce DemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_E_Commerce_DemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_E_Commerce_DemoUITests
-@end
--- README.md
+++
@@ -1,5 +0,0 @@
-# E-Commerce-Demo
-
-Input data can be altered in `Other Files/Input.json`
-
-Cropping function is still left to be built.
